62bec993aa3a7dfa2dac21a56dbfa4dff44a4db8
Miraubolant <victor@mirault.com>
2026-02-19 14:05:15 +0100

    Initial commit: annuaire-logements.fr

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.php",
+    "./pages/**/*.php",
+    "./templates/**/*.php",
+    "./components/**/*.php"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fffbeb',
+          100: '#fef3c7',
+          200: '#fde68a',
+          300: '#fcd34d',
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309',
+          800: '#92400e',
+          900: '#78350f',
+        },
+        accent: {
+          400: '#fb923c',
+          500: '#f97316',
+          600: '#ea580c',
+        }
+      }
+    }
+  },
+  plugins: []
+}
